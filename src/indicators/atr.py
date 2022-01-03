06cda8631b253f0ae0ddfec7cb4b0f8add64395e
joelzhangqc <joel.qc.ca@gmail.com>
2022-01-02 19:28:56 -0500

    add atr indicator.

---
+++ src/indicators/atr.py
@@ -0,0 +1,20 @@
+import pandas as pd
+import talib
+from yahoo_fin.stock_info import get_data
+
+
+def atr(data: pd.DataFrame, period: int, factor=1.5):
+    data[f"atr_{period}"] = talib.ATR(data["high"], data["low"], data["close"], timeperiod=period) * factor
+
+
+if __name__ == '__main__':
+    pd.options.display.max_rows = 2000
+    pd.options.display.max_columns = 200
+    pd.set_option('max_colwidth', 120)
+    pd.options.display.width = 2080
+
+    _hist_data = get_data('tsla', interval="1d")
+
+    atr(data=_hist_data, period=21)
+
+    print(_hist_data.tail(10))
